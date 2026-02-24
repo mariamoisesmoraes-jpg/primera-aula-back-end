3429e0e56887772a9d4303b6f5858769d9e2aed1
mariamoisesmoraes-jpg <maria.moises.moraes@escola.pr.gov.br>
2026-02-24 09:55:34 -0300

    Add files via upload

---
+++ media_se.js
@@ -0,0 +1,14 @@
+let aluno= "Robson";
+let n1= 99;
+let nz= 100;
+let n3= 99;
+let n4= 100;
+let media= 0;
+let result= " ";
+media= ( )/4;
+if (media>=70){
+    result= "aprovado"
+} eles {
+    result= " reprovado"
+}
+console.log ("O aluno"+ aluno+ "tem a meida"+ madia + "e esta:" "+ result+");
