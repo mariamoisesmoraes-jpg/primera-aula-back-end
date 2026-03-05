8575e788b26e766ee6203e50152e74f554f41708
mariamoisesmoraes-jpg <maria.moises.moraes@escola.pr.gov.br>
2026-03-05 09:47:33 -0300

    Add files via upload

---
+++ usandobreach.js
@@ -0,0 +1,9 @@
+const ferramentas = ["martelo", "fita isolante", "chave", "alicate", "trena", "chave de fenda"];
+
+for (let i = 0; i < ferramentas.length; i++) {
+    console.log("verificando: " + ferramentas[i]);
+    if (ferramentas[i] === "chave") {
+        console.log("*** Achei a ferramenta! Parando a busca");
+        break;
+    }
+}
